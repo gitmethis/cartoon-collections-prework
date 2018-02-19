c67dccb9b41dddf3f379d9e061a930a438bd3c56
Chase <chaseit12@gmail.com>
2018-02-19 23:38:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def summon_captain_planet(words)# code an argument here
   words
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
   
   
