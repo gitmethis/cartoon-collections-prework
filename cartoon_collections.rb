6a37eace8d7e7e573e19d180aea17333e1631ef8
Chase <chaseit12@gmail.com>
2018-02-19 23:33:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,10 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   
-  
+  dwarves.each{
+    |dwarf|
+    puts dwarf
+  }
 end
 
 def summon_captain_planet# code an argument here
